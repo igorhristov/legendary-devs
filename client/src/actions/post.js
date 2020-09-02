c69163ac9911e9b219cd9852da102891ee8bfc5d
igorhristov <igorhristov@sourcemxacademy.mk>
2020-09-02 14:10:24 +0200

    Deleting Posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 // Get posts
 
@@ -34,7 +34,6 @@ export const addLike = (id) => async (dispatch) => {
             payload: { id, likes: res.data },
         });
     } catch (err) {
-
         dispatch({
             type: POST_ERROR,
             payload: {
@@ -42,7 +41,6 @@ export const addLike = (id) => async (dispatch) => {
                 status: err.response.status,
             },
         });
-
     }
 };
 
@@ -57,7 +55,6 @@ export const removeLike = (id) => async (dispatch) => {
             payload: { id, likes: res.data },
         });
     } catch (err) {
-
         dispatch({
             type: POST_ERROR,
             payload: {
@@ -65,6 +62,28 @@ export const removeLike = (id) => async (dispatch) => {
                 status: err.response.status,
             },
         });
-        
+    }
+};
+
+// Delete Post
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        const res = await axios.delete(`/api/posts/${id}`);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: id,
+        });
+
+        dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status,
+            },
+        });
     }
 };
